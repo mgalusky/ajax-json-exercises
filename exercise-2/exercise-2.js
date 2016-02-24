7312dab4b1815871d1dbca4426c3681736fa05b2
Melissa Galusky <melissa.galusky@gmail.com>
2016-02-23 19:10:54 -0600

    Preparing to publish

--- exercise-2/exercise-2.js
+++ exercise-2/exercise-2.js
@@ -1,1 +1,13 @@
-// Write your code here!
+$(document).ready(function(){
+
+  var groceryTemplate = $('template#grocery').html();
+
+  $.get('http://portal.batchacademy.com/api/wdfne/test-data/items', funtion(responseData){
+    // declare template variable
+    var newItem = $(groceryTemplate);
+
+    // 
+    $('.title span', newItem).text(products[i].name);
+    $('.price span', newItem).text(products[i].name);
+  });
+});
